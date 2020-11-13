c41d81ad8abd4224f0fe67a942f7972e2f3a0a61
Razputin <razputin666@live.com>
2020-11-13 21:38:03 +0100

    Allt
    Jag la till projektet. <@:o)

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Vector3 pos;
+
+    public float speed;
+    Camera cam;
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            pos.y += speed * Time.deltaTime; 
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            pos.y -= speed * Time.deltaTime; 
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            pos.x -= speed * Time.deltaTime; 
+        }
+        cam.transform.position = pos;
+    }
+}
---
+++ Assets/Scripts/Generator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    public int width;
+    public int height;
+    public int distance;
+    public int space;
+
+    public GameObject Grass;
+    public GameObject Dirt;
+    public GameObject Stone;
+
+    public float heightpoint;
+    public float heightpoint2;
+
+    void Start ()
+    {
+        Generation();
+    }
+
+    void Generation()
+    {
+        distance = height;
+        for (int w=0; w<width; w++)
+        {
+            int lowernum = distance - 1;
+            int heighernum = distance + 2;
+            distance = Random.Range(lowernum, heighernum);
+            space = Random.Range(12, 20);
+            int stonespace = distance - space;
+
+            for (int j=0; j<stonespace; j++)
+            {
+                Instantiate(Stone, new Vector3(w, j), Quaternion.identity);
+            }
+            for (int j=stonespace; j<distance; j++)
+            {
+                Instantiate(Dirt, new Vector3(w, j), Quaternion.identity);
+            }
+            Instantiate(Grass, new Vector3(w, distance), Quaternion.identity);
+        }
+    }
+}
