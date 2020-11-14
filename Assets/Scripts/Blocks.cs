b13a615d22b3c080ce9110995c27f9c719167e96
Christopher Nylen <Joffynylen@hotmail.com>
2020-11-14 05:28:12 +0100

    JUMPY GUY XD

---
+++ Assets/Scripts/Blocks.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blocks : MonoBehaviour
+{
+    CameraMovement playerCamera;
+
+    private void OnEnable()
+    {
+        playerCamera = new CameraMovement();
+
+        //   playerCamera.transform.position - gameObject.transform.position;
+
+       
+        
+
+    }
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed;                //Floating point variable to store the player's movement speed.
+    [SerializeField]
+    LayerMask blockLayerMask;
 
+    public float speed;                //Floating point variable to store the player's movement speed.
+    public float jumpVelocity;
     private Rigidbody2D rb2d;        //Store a reference to the Rigidbody2D component required to use 2D Physics.
+    private BoxCollider2D boxCollider2d;
 
     // Use this for initialization
     [SerializeField]
@@ -17,6 +21,7 @@ public class PlayerController : MonoBehaviour
     {
         //Get and store a reference to the Rigidbody2D component so that we can access it.
         rb2d = GetComponent<Rigidbody2D>();
+        boxCollider2d = transform.GetComponent<BoxCollider2D>();
        
     }
 
@@ -35,6 +40,24 @@ public class PlayerController : MonoBehaviour
         //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
         rb2d.AddForce(movement * speed);
         _camera.transform.position = new Vector3(rb2d.transform.position.x, rb2d.transform.position.y, -1);
+        if (Input.GetKey(KeyCode.Space) && IsGrounded())
+        {
+            rb2d.velocity = Vector2.up * jumpVelocity;
+        }
+
+        Debug.Log(IsGrounded());
     }
+
+    bool IsGrounded()
+    {
+       RaycastHit2D raycastHit2D = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down, .1f);
+       // Debug.Log(raycastHit2D.collider);
+        return raycastHit2D.collider != null;
+        
+             
+        
+    }
+
+
 }
 
