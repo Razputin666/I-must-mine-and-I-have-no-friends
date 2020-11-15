227df759db45c9d04b8b6c83c2ca6ef85da80e25
Razputin <razputin666@live.com>
2020-11-15 04:18:56 +0100

    Värdelösa animationer

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,6 +17,7 @@ public class PlayerController : MonoBehaviour
     public float jumpVelocity;
     private Rigidbody2D rb2d;        //Store a reference to the Rigidbody2D component required to use 2D Physics.
     private BoxCollider2D boxCollider2d;
+    private bool facingRight;
     Vector3 worldPosition;
 
     public Queue<IEnumerator> coroutineQueue = new Queue<IEnumerator>();
@@ -28,6 +29,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         //Get and store a reference to the Rigidbody2D component so that we can access it.
+        facingRight = true;
         rb2d = GetComponent<Rigidbody2D>();
         boxCollider2d = transform.GetComponent<BoxCollider2D>();
         SetMiningMode(); // Vi har inte combat än så den e på mining default
@@ -37,14 +39,17 @@ public class PlayerController : MonoBehaviour
     //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
     void FixedUpdate()
     {
+        
         //Store the current horizontal input in the float moveHorizontal.
         float moveHorizontal = Input.GetAxis("Horizontal");
-
+        Flip(moveHorizontal);
         //Store the current vertical input in the float moveVertical.
         float moveVertical = Input.GetAxis("Vertical");
 
         //Use the two store floats to create a new Vector2 variable movement.
         Vector2 movement = new Vector2(moveHorizontal, 0);
+
+        
 
         //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
         rb2d.AddForce(movement * speed);
@@ -87,6 +92,7 @@ public class PlayerController : MonoBehaviour
        // Debug.Log(raycastHit2D.collider);
         return raycastHit2D.collider != null;
     }
+
     #region GameLogic
     public void SetMiningMode()
     {
@@ -124,4 +130,15 @@ public class PlayerController : MonoBehaviour
     }
 
     #endregion
+    private void Flip(float horizontal)
+    {
+        if(horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
+        {
+            facingRight = !facingRight;
+
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+    }
 }
