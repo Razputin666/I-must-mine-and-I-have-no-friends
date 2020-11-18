4d33902bf64b7530c4915d818c1f0675de39ee79
Christopher Nylen <Joffynylen@hotmail.com>
2020-11-18 21:20:52 +0100

    Nyttstruff

---
+++ Assets/GamemanagerJoffy.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Unity.Entities;
+using Unity.Mathematics;
+
+
+public class GamemanagerJoffy : MonoBehaviour
+{
+
+    public static GamemanagerJoffy main;
+
+    public GameObject notSure;
+
+    public float xBound = 3f;
+    public float yBound = 3f;
+    public float ballSpeed = 3f;
+    public float bruh = 2f;
+    public int[] somethingCool;
+
+    public Text mainText;
+    public Text[] playerTexts;
+
+    Entity ballEntityPrefab;
+    EntityManager manager;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        if (main != null && main != this)
+        { 
+            Destroy(gameObject);
+        return;
+        }
+        main = this;
+
+
+
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/JoffysTestScript/ActualEntityStuff/PlayerInputEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+[GenerateAuthoringComponent]
+public struct PlayerInputEntity : IComponentData
+{
+    public KeyCode leftKey;
+    public KeyCode rightKey;
+    public KeyCode jumpKey;
+}
